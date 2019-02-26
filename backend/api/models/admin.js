fc89c890d4c721c4adfb05c271c1d309c1c13b33
lawrenceagles <eagles@codewitheagles.com>
2019-02-26 09:28:27 +0100

    added company model

---
+++ backend/api/models/admin.js
@@ -0,0 +1,96 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
+
+const Schema = mongoose.Schema;
+const role = ["super_admin", "manager", "observer"];
+
+const AdminSchema = new Schema({
+    firstname: {
+        type: String,
+        required: [true, 'First Name is required'],
+        maxlength: 120,
+        trim: true,
+    },
+    lastname: {
+        type: String,
+        required: [true, 'Last Name is required'],
+        maxlength: 120,
+        trim: true
+    }, 
+    username: {
+        type: String,
+        required: [true, 'Username is required'],
+        maxlength: 120,
+        trim: true,
+        unique: true
+     },
+    email: {
+        type: String,
+        required: [true, 'email is required'],
+        trim: true,
+        unique: true,
+        validate: {
+            validator: validator.isEmail,
+            message: '{VALUE }is not a valid Email!'
+        }
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minlength: 6,
+      default: "lawrenceagles1235"
+    },
+    phone: {
+        type: String,
+        validate: {
+          validator: function(v) {
+            return /\d{3}-\d{3}-\d{3}-\d{4}/.test(v);
+          },
+          message: props => `${props.value} is not a valid phone number!`
+        },
+        required: [true, 'User phone number required']
+      },
+    role:{
+      type: String,
+      trim: true,
+      enum: role
+    },
+    tokens: [{
+        access: {
+          type: String,
+          required: true
+        },
+        token: {
+          type: String,
+          required: true
+        }
+    }]
+});
+
+// encrypt password using bcrypt conditionally. Only if the user is newly created or he updated his password directly.
+AdminSchema.pre('save', function(next){
+  let admin = this;
+  if(admin.$isDefault('password') && !admin.isModified('password')){
+    bcrypt.genSalt(10, (err, salt)=> {
+      bcrypt.hash(admin.password, salt, (err, hash)=> {
+        admin.password = hash; // overwrite the password with the harsh value before saving to the DB.
+        console.log(admin.isModified('password'));
+        next();
+      })
+    })
+  }else if(admin.isDirectModified('password') && !admin.$isDefault('password')) {
+    bcrypt.genSalt(10, (err, salt)=> {
+      bcrypt.hash(admin.password, salt, (err, hash)=> {
+        admin.password = hash; // overwrite the password with the harsh value before saving to the DB.
+        next();
+      })
+    })
+  }
+  else{
+    // call next and move to execute the next action if password is already harshed.
+    next();
+  }
+});
+
+module.exports = mongoose.model('Admin', AdminSchema);
