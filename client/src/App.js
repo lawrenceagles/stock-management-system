a13e50af699768ec8e95fa53da97ddfcea0236b1
Paul Ayuk <ayukpaul1@gmail.com>
2019-03-05 11:39:11 +0100

    login and dashboard styles added

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
   render() {
@@ -11,9 +9,7 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
-          <p>
-            Sup!.
-          </p>
+          <p>Sup!.</p>
           <a
             className="App-link"
             href="https://reactjs.org"
