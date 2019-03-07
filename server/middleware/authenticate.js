672bd5ba9214b222497591af86355167c1c651b8
lawrenceagles <eagles@codewitheagles.com>
2019-03-07 16:49:56 +0100

    tested user and admin authentication 100% working

--- server/middleware/authenticate.js
+++ server/middleware/authenticate.js
@@ -4,29 +4,29 @@ const {User} = require('../api/models/user');
 let authenticate = (req, res, next) => {
     let token = req.header('x-auth'); // grap the token from the server
     if(Admin || !User){
-    //     Admin.findByToken(token).then((admin) => { // model method to find admin by token
-    //     if(!admin) { // reject the promise if no admin is found
-    //         return Promise.reject();
-    //         // you can redirect the user to the login page!
-    //     }
-    //     req.admin = admin;
-    //     req.token = token;
+        Admin.findByToken(token).then((admin) => { // model method to find admin by token
+        if(!admin) { // reject the promise if no admin is found
+            return Promise.reject();
+            // you can redirect the user to the login page!
+        }
+        req.admin = admin;
+        req.token = token;
 
-    //     // Role management
-    //     if(req.method !== 'GET' && req.admin.role === 'observer'){
-    //          return Promise.reject();
-    //         // you can redirect the user to the login page!
-    //     }
+        // Role management
+        if(req.method !== 'GET' && req.admin.role === 'observer'){
+             return Promise.reject();
+            // you can redirect the user to the login page!
+        }
 
-    //     if(req.method === 'POST' && req.admin.role === 'manager') {
-    //         return Promise.reject();
-    //         // you can redirect the user to the login page!
-    //     }
+        if(req.method === 'POST' && req.admin.role === 'manager') {
+            return Promise.reject();
+            // you can redirect the user to the login page!
+        }
 
-    //     next();
-    // }).catch((e) => {
-    //     res.status(401).send("You do not have the permission you perform this operation");
-    // });
+        next();
+    }).catch((e) => {
+        res.status(401).send("You do not have the permission you perform this operation");
+    });
     }
     if(User || !Admin){
          User.findByToken(token).then((user)=>{
