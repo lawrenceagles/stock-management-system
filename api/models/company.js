43a5adc1411783c1ad70c1f433bed5c56fcdb756
lawrenceagles <eagles@codewitheagles.com>
2019-04-02 17:06:02 +0100

    updated schemebatch

--- api/models/company.js
+++ api/models/company.js
@@ -96,8 +96,7 @@ const companySchema = new Schema({
     },
     schemeBatch:[{
          name:{
-            type: String,
-            unique: true
+            type: String
         },  
         allocatedShares: {
             type: Number
