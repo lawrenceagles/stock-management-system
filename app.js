1fbc54ed33896a08ea044293cde72328a6a91f0e
lawrenceagles <eagles@codewitheagles.com>
2019-04-04 13:52:53 +0100

    commited modified app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const cron = require('node-cron');
 const bodyParser = require('body-parser');
 const route = require('./api/routes/Admin');
+const multer = require('multer')
 const userRoute = require('./api/routes/users');
 const companyRoute = require('./api/routes/company');
 const router = express.Router();
@@ -14,25 +15,6 @@ const {mongoose} = require('./config/db/mongoose');
 
 const app = express(); // create express app and store it in the app variable
 app.use(cors());
-
-const multer = require('multer')
-const upload = multer({
-    dest: 'scheme_rules',
-    limits: {
-        fileSize: 3000000
-    },
-    fileFilter(req, file, cb) {
-        if (!file.originalname.match(/\.(pdf|PDF)$/)) {
-            return cb(new Error('Please upload a pdf document'));
-        }
-
-        cb(undefined, true)
-    }
-})
-app.post('/upload', upload.single('upload'), (req, res) => {
-    res.send("Scheme rule uploaded successfully");
-})
-
 
 //support parsing of application/x-www-form-urlencoded post data
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -60,6 +42,24 @@ app.use((req, res, next)=>{
 // cron.schedule('* * * * *', () => {
 //   vestingDateAuto(Date.now());
 // });
+
+// configure multer for file upload
+const upload = multer({
+    dest: 'scheme_rules',
+    limits: {
+        fileSize: 3000000
+    },
+    fileFilter(req, file, cb) {
+        if (!file.originalname.match(/\.(pdf|PDF)$/)) {
+            return cb(new Error('Please upload a pdf document'));
+        }
+
+        cb(undefined, true)
+    }
+})
+app.post('/upload', upload.single('upload'), (req, res) => {
+    res.send("Scheme rule uploaded successfully");
+})
 
 app.listen(Port, () => {
     console.log(`${Host} server started on ${Port}`);
